bbfd7290b4c4ba72fb40bf7bb1733db5dc41707f
Rey Thompson <rthompson1@gmail.com>
2015-05-28 23:45:07 -0500

    second commit

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -22,3 +22,71 @@ require("babel/register")
     // new Router()
 // }
 
+var clock = () => {
+	var now = new Date(
+	var text = '${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}'
+	document.querySelector('.container').innerHTML = text
+
+
+
+	var HMS = [now.getHours(), now.getMinutes(), now.getSeconds()]
+	var range = [24, 60, 60]
+	var values  = HMS.map((v, i) => Math.floor(v/range[i]*255))
+	var color = 'rgb(${value.join(',')})'
+	document.body.style['background-color'] = color
+}
+
+setInterval(clock. 1000)
+
+
+//convert binary to hex
+
+//option 1
+
+//hexString = yourNumber.toString(16);
+//yourNumber = parseInt(hexString, 16);
+
+//option 2
+
+var binhex = {
+
+	convertBinarytoHexadecimal: function(binaryString)
+{
+	var res = '';	
+	for (var i=0, i < binaryString.length; i+=4)
+	{
+		var bytes = binaryString.substri(i, 4);
+		var decimal = parseInt(bytes, 2);
+		var hex = decimal.toString(16);
+		res += hex.toUpperCase();
+	}
+	return res;
+},
+
+	convertHexadecimalToBinary: function(hexString)
+{
+	var res = '';
+	for (var i = 0; i < hexString.length; i++)
+	{
+		var decimal = parseInt(hexString.charAt(i), 16);
+		var binary = this.leftPadding(decimal.toString(2), '0', 4);
+		res += binary;
+		}
+
+	return res;
+};
+
+	leftPadding: function(inputString, padCharater, totalCharacters)
+{
+	if ( !inputString || !padCharater || inputString.length	>=totalCharacters)
+	{
+		return inputString;
+	}
+
+	var charsToAdd = (totalCharacters - inputString.length)/padCharater.length;
+	for (var i = 0; i < charsToAdd; i++)
+	{
+		inputString = padCharater + inputString;
+		}
+	return inputString;
+};
