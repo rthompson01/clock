13d678c56647b728b9325bfbab90e0c8713f5d53
Rey Thompson <rthompson1@gmail.com>
2015-06-11 17:32:11 -0500

    updating code, in process

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -9,39 +9,24 @@ require('whatwg-fetch') //--> not a typo, don't store as a var
 // es6 polyfills, powered by babel
 require("babel/register")
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
-
-// require your own libraries, too!
-// var Router = require('./app.js')
-
-// window.addEventListener('load', app)
-
-// function app() {
-// start app
-// new Router()
-// }
-
 var clock = () => {
-	// debugger
-    var now = new Date()
+    var now = new Date();
+    var clock = `${now.getHours()}:${ now.getMinutes()}:${ now.getSeconds()}`
+    var minutes = (now.getMinutes().toString().length==1) ? "0" + d.getMinutes():d.getMinutes()
+    var seconds = (now.getSeconds().toString().length==1) ? "0" + d.getSeconds():d.getSeconds()
+    var correct = `${now.getHours()}:${minutes}:${seconds}`
+    document.querySelector(".container").innerHTML = correct; 
+    
+
     var HMS = [now.getHours(), now.getMinutes(), now.getSeconds()]
-
-    var text = `${HMS[0]}:${HMS[1]}:${HMS[2]}`
-    document.querySelector('.container').innerHTML = text
-
     var range = [24, 60, 60]
     var values = HMS.map((v, i) => Math.floor(v / range[i] * 255))
-    // var color = `rgb(${values.join(',')})`
-    var color = '#'+values.map((v) => v.toString(16)).join('')
-    console.log(color)
-    document.body.style['background-color'] = color
+    var color = `rgb(${values.join(',')})`
+    document.body.style['background'] = color
+    var S = `${3*d.getSeconds()}px`
+    document.body.querySelector('hr').style['width'] = S
 
-
-    document.querySelector('.hexadecimal').innerHTML = values.map((v) => v.toString(16)).join(':')
-
-    document.querySelector('.container hr".style.width')
-
+document.querySelector(".hovercontainer").innerHTML =  values.map((v) => v.toString(16)).join(':');
+    
 }
-
-setInterval(clock,1000)
+setInterval(clock, 1000)
